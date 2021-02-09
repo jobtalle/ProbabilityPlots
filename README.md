540aee93cb1911cf2b936b998c87cbe450ad43d5
Job <jobtalle@hotmail.com>
2021-02-09 16:19:51 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Probability plots
 
-Plotting probabilities
+[Works in a browser!](https://jobtalle.com/ProbabilityPlots)
+
+Plotting the probabilities a function with random input returns a number within a certain range.
+
+![alt text](preview.png "Probability plots")
--- swappedFunction.js
+++ swappedFunction.js
@@ -9,7 +9,7 @@ const SwappedFunction = function(fy) {
     return this.evaluate.bind(this);
 };
 
-SwappedFunction.prototype.STEPS = 512;
+SwappedFunction.prototype.STEP = 1 / 1024;
 
 /**
  * Evaluate
@@ -17,21 +17,10 @@ SwappedFunction.prototype.STEPS = 512;
  * @returns {number} The X value
  */
 SwappedFunction.prototype.evaluate = function(y) {
-    if (y === 0)
-        return 0;
-    else if (y === 1)
-        return 1;
+    let x = 0;
 
-    let x = .5;
-
-    for (let i = 0; i < this.STEPS; ++i) {
-        const step = 1 / (i + 2);
-
-        if (this.fy(x) > y)
-            x -= step;
-        else
-            x += step;
-    }
+    while (y > this.fy(x))
+        x += this.STEP;
 
     return x;
 };
